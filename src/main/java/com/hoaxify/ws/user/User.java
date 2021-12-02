9bbe8b0f58775be9751de1c48c31db6784642e59
Guven Ayvazoglu <gayvazoglu.29@gmail.com>
2021-12-02 19:45:55 +0300

    last upload

---
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -0,0 +1,11 @@
+package com.hoaxify.ws.user;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String username;
+    private String displayName;
+    private String password;
+}
---
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -0,0 +1,20 @@
+package com.hoaxify.ws.user;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    @CrossOrigin
+    @PostMapping("/api/1.0/Users")
+    public void createUser(@RequestBody User user){
+        log.info(user.toString());
+    }
+}
