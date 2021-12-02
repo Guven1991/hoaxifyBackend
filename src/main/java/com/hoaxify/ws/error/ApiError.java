e8b9484b3716267701b14859146d76687f031fb6
Guven Ayvazoglu <gayvazoglu.29@gmail.com>
2021-12-02 21:56:50 +0300

    upload form username and ApiError

---
+++ src/main/java/com/hoaxify/ws/error/ApiError.java
@@ -0,0 +1,21 @@
+package com.hoaxify.ws.error;
+
+import lombok.Data;
+
+import java.util.Date;
+import java.util.Map;
+
+@Data
+public class ApiError {
+    private int status;
+    private String message;
+    private String path;
+    private long timestap = new Date().getTime();
+    private Map<String, String> validationErrors;
+
+    public ApiError(int status, String message, String path) {
+        this.status = status;
+        this.message = message;
+        this.path = path;
+    }
+}
--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,12 +1,14 @@
 package com.hoaxify.ws.user;
 
+import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.GenericResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -23,7 +25,11 @@ public class UserController {
     public ResponseEntity<?> createUser(@RequestBody User user){
         String username = user.getUsername();
 if(username==null || username.isEmpty()){
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    ApiError error = new ApiError(400, "Validation error", "/api/1.0/users");
+    Map<String,String> validationErrors = new HashMap<>();
+    validationErrors.put("username", "Username cannot be null");
+    error.setValidationErrors(validationErrors);
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 }
         userService.save(user);
         return ResponseEntity.ok( new GenericResponse("User Created"));
