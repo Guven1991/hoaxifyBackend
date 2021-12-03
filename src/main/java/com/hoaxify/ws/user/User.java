4167e577f862868ae32bdba8dc90cd559c2bb13d
Guven Ayvazoglu <gayvazoglu.29@gmail.com>
2021-12-03 18:08:04 +0300

    registration form work

--- src/main/java/com/hoaxify/ws/user/User.java
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -1,10 +1,11 @@
 package com.hoaxify.ws.user;
 
-import lombok.Data;
 
+import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
@@ -13,7 +14,11 @@ public class User {
     @Id
     @GeneratedValue
     private long id;
+
+    @NotNull
     private String username;
+
+    @NotNull
     private String displayName;
     private String password;
 }
--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -4,9 +4,11 @@ import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.GenericResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,17 +23,20 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/api/1.0/users")
-    @ResponseStatus(HttpStatus.CREATED) //Postman de (.OK---> 200 ok) yi 201 yapar
-    public ResponseEntity<?> createUser(@RequestBody User user){
-        String username = user.getUsername();
-if(username==null || username.isEmpty()){
-    ApiError error = new ApiError(400, "Validation error", "/api/1.0/users");
-    Map<String,String> validationErrors = new HashMap<>();
-    validationErrors.put("username", "Username cannot be null");
-    error.setValidationErrors(validationErrors);
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
-}
+    public GenericResponse createUser(@Valid @RequestBody User user) {
         userService.save(user);
-        return ResponseEntity.ok( new GenericResponse("User Created"));
+        return new GenericResponse("user created");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST) //Postman de (.OK---> 200 ok) yi 201 yapar
+    public ApiError handleValidationException(MethodArgumentNotValidException exception) {
+        ApiError error = new ApiError(400, "Validation error", "/api/1.0/users");
+        Map<String, String> validationErrors = new HashMap<>();
+        for(FieldError fieldError: exception.getBindingResult().getFieldErrors()){
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        error.setValidationErrors(validationErrors);
+        return error;
     }
 }
